23d2fcb7f25284da60e342552a564381f6f0c034
devtiagomantay <devtiagomantay@gmail.com>
2022-12-18 19:26:17 +0000

    Create functions for triangule type calculation and unit tests

--- triangule-api-serverless/app.py
+++ triangule-api-serverless/app.py
@@ -1,16 +1,24 @@
-from flask import Flask, jsonify, make_response
+import boto3
+
+from flask import Flask, jsonify, make_response, request
+from triangle import calculate_triangle_type
+
+REQUEST_HISTORY_TABLE_NAME = 'requests-history'
 
 app = Flask(__name__)
+client = boto3.client('dynamodb')
 
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+@app.route("/triangle-type", methods=["POST"])
+def triangle_type():
+    # TODO: validation of post body
+    dimensions = request.json.get('dimensions')
+    return jsonify(message=calculate_triangle_type(dimensions))
 
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+@app.route("/requests-history")
+def get_requests_history():
+    return jsonify(message='result')
 
 
 @app.errorhandler(404)
---
+++ triangule-api-serverless/models.py
@@ -0,0 +1,11 @@
+# from flask_login import UserMixin
+#
+# # Class to store the user's request
+#
+#
+# class RequestsHistory(UserMixin):
+# 	def __init__(self):
+# 		self.userId = None
+# 		self.request = None
+# 		self.ts = None
+# 		self.result = None

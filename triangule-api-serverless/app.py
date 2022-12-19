b177656020bf72bf5f55280f5585082e84d146cf
devtiagomantay <55160457+devtiagomantay@users.noreply.github.com>
2022-12-19 22:14:24 +0000

    13 implement the triangule type calculations (#14)
    * Add triangle functions and unit tests
    
    * Create the enpoint to calculate the triangle type and. Add helper function to extract values from the request.
    
    * Update README file
    
    * Add infrastructure diagram
    
    * Update README file

--- triangule-api-serverless/app.py
+++ triangule-api-serverless/app.py
@@ -1,11 +1,19 @@
-from flask import Flask, jsonify, make_response
-
+from flask import Flask, jsonify, make_response, request
+from triangle import calculate_triangle_type
+from helper import get_values_from_request
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+@app.route("/triangle-type", methods=["POST"])
+def triangle_type():
+    # TODO: validation of post body
+    print('Request: ', request.json)
+    dimensions = get_values_from_request(request, 'dimensions')
+    res = 'Error!'
+    if dimensions:
+        res = calculate_triangle_type(dimensions)
+
+    return jsonify(message=res)
 
 
 @app.route("/hello")
---
+++ triangule-api-serverless/triangle_calc_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+def test_calculate_triangle_type():
+	from triangle import calculate_triangle_type
+
+	assert calculate_triangle_type([9,9,9]) == "equilateral"
+	assert calculate_triangle_type([1.1, 1.2, 1.1]) == "isosceles"
+	assert calculate_triangle_type([1, 7, 9]) == "scalene"
+
+
+def test_triangle_equilateral():
+	from triangle import is_triangle_equilateral
+
+	assert is_triangle_equilateral([1.1, 1.2, 1.3]) is False
+	assert is_triangle_equilateral([1.1, 1.1, 1.1]) is True
+
+
+def test_triangle_is_triangle_isosceles():
+	from triangle import is_triangle_isosceles
+
+	assert is_triangle_isosceles([1.1, 1.2, 1.3]) is False
+	assert is_triangle_isosceles([1, 1.1, 1.1]) is True
+	assert is_triangle_isosceles([1, 1.9, 1.5]) is False
+	assert is_triangle_isosceles([1, 7, 9]) is False
+
+
+def test_triangle_is_triangle_scalene():
+	from triangle import is_triangle_scalene
+
+	assert is_triangle_scalene([1.1, 1.2, 1.3]) is True
+	assert is_triangle_scalene([1.1, 1.1, 1.1]) is False
+	assert is_triangle_scalene([1.1, 1.11, 1.11]) is False

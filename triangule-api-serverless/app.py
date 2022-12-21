a835a713f7c9fa5363cb8cc3ef18463ec35f3d70
devtiagomantay <devtiagomantay@gmail.com>
2022-12-21 16:39:27 +0000

    Add error handling for http error codes 400,401,404 and 500

--- triangule-api-serverless/app.py
+++ triangule-api-serverless/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response, request
 from triangle import calculate_triangle_type
 from helper import get_values_from_request
+
 app = Flask(__name__)
 
 
@@ -9,7 +10,7 @@ def triangle_type():
     # TODO: validation of post body
     print('Request: ', request.json)
     dimensions = get_values_from_request(request, 'dimensions')
-    res = 'Error!'
+    res = '
     if dimensions:
         res = calculate_triangle_type(dimensions)
 
@@ -21,6 +22,25 @@ def hello():
     return jsonify(message='Hello from path!')
 
 
+@app.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@app.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
 @app.errorhandler(404)
-def resource_not_found(e):
+def resource_not_found(_error):
+    """Return a http 500 error to client"""
     return make_response(jsonify(error='Not found!'), 404)
+
+
+@app.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return make_response(jsonify({'error': 'Server error'}), 500)

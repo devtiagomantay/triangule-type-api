21d18f808a4a1504ee0a84bba7444a994124f770
devtiagomantay <55160457+devtiagomantay@users.noreply.github.com>
2022-12-21 23:52:27 +0000

    6 create validation for input files (#17)
    * Add payload validation with schema and methods
    
    * Fix validation method
    
    * Change schema from file to variable in app file
    
    * Adjust api response

--- triangule-api-serverless/app.py
+++ triangule-api-serverless/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, make_response, request
 from triangle import calculate_triangle_type
-from helper import get_values_from_request
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_expects_json import expects_json
+
+from helper import get_payload_dimensions
 
 app = Flask(__name__)
 
-
+# Swagger configuration
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
@@ -17,35 +19,53 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
+dimensions_schema = {
+    "type": "object",
+    "properties": {
+        "dimensions": {"type": "array"}
+    },
+    "required": ["dimensions"]
+}
+
 
 @app.route("/triangle-type", methods=["POST"])
+@expects_json(dimensions_schema)
 def triangle_type():
-    # TODO: validation of post body
-    print('triangle-type endpoint Request: ', request.json)
-    dimensions = get_values_from_request(request, 'dimensions')
-    res = ''
-    if dimensions:
-        res = calculate_triangle_type(dimensions)
+    """Calculates the triangle type based on the dimensions"""
+    try:
+        print('triangle-type endpoint request: ', request.json)
+        dimensions = get_payload_dimensions(request, 'dimensions')
+        if dimensions:
+            res = calculate_triangle_type(dimensions)
+            return make_response(jsonify(message=res))
 
-    return make_response(jsonify(message=res), 200)
+        return make_response(jsonify(message='Payload is invalid'))
+    except BaseException as e:
+        print('Unknown error on triangle-type endpoint. Detail: ' + str(e))
+        return make_response(jsonify(message='Unknown error'))
+
+
+@app.route("/hello")
+def hello():
+    return jsonify(message='Hello from path!')
 
 
 @app.errorhandler(400)
-def handle_400_error(_error):
+def bad_request_error(_error):
     """Return a http 400 error to client"""
-    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+    return make_response(jsonify({'error': 'Bad request. Check the payload'}), 400)
 
 
 @app.errorhandler(401)
-def handle_401_error(_error):
+def unauthorised_error(_error):
     """Return a http 401 error to client"""
     return make_response(jsonify({'error': 'Unauthorised'}), 401)
 
 
 @app.errorhandler(404)
 def resource_not_found(_error):
-    """Return a http 500 error to client"""
-    return make_response(jsonify(error='Not found!'), 404)
+    """Return a http 404 error to client"""
+    return make_response(jsonify({'error': 'Not found!'}), 404)
 
 
 @app.errorhandler(500)
